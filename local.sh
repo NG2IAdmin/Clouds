c7392a518de2f914e4e2e13a7ea9e27af9a64896
ng2iadmin <hemant@sharmas.biz>
2015-10-31 11:11:38 +0530

    changes

--- local.sh
+++ local.sh
@@ -3,7 +3,6 @@
 # List of arguments: StartingBlockNr TotalBlocks InFileName OutFileName MaskWidth PeakWidth
 
 NCPUS=$( cat /proc/cpuinfo | grep processor | wc -l )
-echo "Computing locally on " $NCPUS
 for ((i=$1;i<=NCPUS+$1;i++))
 do
 	bin/iojpegparts $3 $4 $5 $6 $i $2 &
